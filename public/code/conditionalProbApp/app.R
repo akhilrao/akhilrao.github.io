ed2e0b9c5fe197e6447b870cd8159da380a90092
akhilrao <akhilrao72@gmail.com>
2016-02-07 15:06:20 -0700

    new post

---
+++ public/code/conditionalProbApp/app.R
@@ -0,0 +1,41 @@
+# conditionalProbApp
+
+library(shiny)
+
+ui <- fluidPage(
+  headerPanel('Conditional probability comparison'),
+  sidebarPanel('Input parameters for events of interest. ',
+    numericInput(inputId="event.a",label='Occurrences of event A', 150,
+      min = 0),
+    numericInput(inputId="event.b", label='Occurrences of event B', 500,
+                 min = 0),
+    numericInput(inputId="population", label='Population size', 1000,
+                 min = 0),
+    h4("Comparison event (red line): probability(died in USA in 2010|85 or older in USA in 2010)"),
+    h6("Number of deaths in USA in 2010 (event A): 2,550,000"),
+    h6("Population over 85 in USA in 2010 (event B): 5,176,000"),
+    h6("Total US population in 2010 (population size): 310,000,000"),
+    h6("P(A|B)=P(B|A)*(P(A)/P(B))")
+  ),
+  mainPanel(
+    plotOutput(outputId="plot1")
+  )
+)
+
+server <- function(input, output) {
+  
+  P.b.given.a <- seq(0,1,by=0.01)
+  ones <- rep(1,length=length(P.b.given.a))
+  baseline.pa <- 2550000/310000000
+  baseline.pb <- 5176000/310000000
+  baseline <- pmin(P.b.given.a*(baseline.pa/baseline.pb),ones)
+  
+  output$plot1 <- renderPlot({
+    plot(pmin(P.b.given.a*((input$event.a/input$population)/(input$event.b/input$population)),ones),type="l",main="Probability of A given B", xlab="Assumed P(B|A)", ylab="P(A|B)",xaxt="n", ylim=c(0,1))
+ axis(1,at=1:101,labels=P.b.given.a)
+    lines(baseline,col=2)
+     })
+
+}
+
+shinyApp(ui = ui, server = server)
